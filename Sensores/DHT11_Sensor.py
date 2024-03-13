449e2a8b7a467c9a8e5c193647cc98e989056afa
RicardoGraziato Caso <ricardograziato@gmail.com>
2024-03-13 19:17:31 +0000

    Adicionando tratamento de exceção e registro de logs

--- Sensores/DHT11_Sensor.py
+++ Sensores/DHT11_Sensor.py
@@ -3,6 +3,7 @@ import Adafruit_DHT
 import time
 from datetime import datetime
 import RPi.GPIO as GPIO
+import logging
 
 pin = 16
 botao = 20
@@ -12,6 +13,8 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) #.BCM numeração das GPIO /.BOARD numeraçao da placa
 
 GPIO.setup(20, GPIO.IN)
+
+logging.basicConfig(filename='DHT11.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def criarTabela():
     nome_mes = datetime.now().strftime('%B').lower()
@@ -35,22 +38,25 @@ def inserirDados(data, hora, temperatura):
 criarTabela()
        
 while True:
-    if GPIO.input(botao) == GPIO.HIGH:
-        tempo_inicial = time.time()
-        while (time.time() - tempo_inicial) < 1800:  #duração da execução em segundos (30 minutos = 1800)
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            current_date = now.strftime("%d-%m-%Y")
-            temperatura = Adafruit_DHT.read_retry(sensor,pin)[1]
+    try:
+        if GPIO.input(botao) == GPIO.HIGH:
+            tempo_inicial = time.time()
+            while (time.time() - tempo_inicial) < 1800:  #duração da execução em segundos (30 minutos = 1800)
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                current_date = now.strftime("%d-%m-%Y")
+                temperatura = Adafruit_DHT.read_retry(sensor,pin)[1]
+                
+                inserirDados(current_date, current_time, temperatura)
             
-            inserirDados(current_date, current_time, temperatura)
-        
-            print('Data:', current_date)
-            print('Hora:', current_time)
-            print('Temperatura: {0:0.1f}C'.format(temperatura))
-            print(" ")
-            print("temperatura salva")
-            time.sleep(15) #Tempo entre as consultas/leituras
-        print("fim da execução.")
-        fim = "Fim da execução"
-        inserirDados(fim, fim, 0) #Registra um "separador" no banco de dados para facilitar compreensão
+                print('Data:', current_date)
+                print('Hora:', current_time)
+                print('Temperatura: {0:0.1f}C'.format(temperatura))
+                print(" ")
+                print("temperatura salva")
+                time.sleep(15) #Tempo entre as consultas/leituras
+            print("fim da execução.")
+            fim = "Fim da execução"
+            inserirDados(fim, fim, 0) #Registra um "separador" no banco de dados para facilitar compreensão
+    except Exception as e:
+        logging.error('Ocorreu um erro: %s', str(e))
--- Sensores/DS18B20_Sensor.py
+++ Sensores/DS18B20_Sensor.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 import time
 from w1thermsensor import W1ThermSensor
+import logging
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -12,6 +13,8 @@ botao = 20
 sensor = W1ThermSensor()
 
 GPIO.setup(botao, GPIO.IN)
+
+logging.basicConfig(filename='DS18B20.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def criarTabela():
     nome_mes = datetime.now().strftime('%B').lower()
@@ -35,22 +38,25 @@ def inserirDados(data, hora, temperatura):
 criarTabela()
        
 while True:
-    if GPIO.input(botao) == GPIO.HIGH:
-        tempo_inicial = time.time()
-        while (time.time() - tempo_inicial) < 1800:
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            current_date = now.strftime("%d-%m-%Y")
-            temperatura = sensor.get_temperature()
+    try:
+        if GPIO.input(botao) == GPIO.HIGH:
+            tempo_inicial = time.time()
+            while (time.time() - tempo_inicial) < 1800:
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                current_date = now.strftime("%d-%m-%Y")
+                temperatura = sensor.get_temperature()
+                
+                inserirDados(current_date, current_time, temperatura)
             
-            inserirDados(current_date, current_time, temperatura)
-        
-            print('Data:', current_date)
-            print('Hora:', current_time)
-            print('Temperatura: {0:0.1f}C'.format(temperatura))
-            print(" ")
-            print("Temperatura salva")
-            time.sleep(15)
-        print("Fim da execução.")
-        fim = "Fim da execução"
-        inserirDados(fim, fim, 0)
+                print('Data:', current_date)
+                print('Hora:', current_time)
+                print('Temperatura: {0:0.1f}C'.format(temperatura))
+                print(" ")
+                print("Temperatura salva")
+                time.sleep(15)
+            print("Fim da execução.")
+            fim = "Fim da execução"
+            inserirDados(fim, fim, 0)
+    except Exception as e:
+        logging.error('Ocorreu um erro: %s', str(e))
